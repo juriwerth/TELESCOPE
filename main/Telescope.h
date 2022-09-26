5cad8ee955db8fc51d238c179d17c7fd4631ab38
Juri Werth <juriwerth1@gmail.com>
2022-09-26 10:50:39 +0200

    Update Telescope.h

--- main/Telescope.h
+++ main/Telescope.h
@@ -11,17 +11,20 @@ inline long stepsPitch;
 inline long stepsYaw;
 inline float deltaPitch;
 inline float deltaYaw;
+inline bool prep = false;
 const inline String seperator = " ";
 
 const inline int pulsYaw = 2;
 const inline int dirYaw = 3;
-const inline int pulsPitch = 5;
-const inline int dirPitch = 6;
-const inline int switchPin = 12;
+const inline int pulsPitch = 4;
+const inline int dirPitch = 5;
+const inline int switch0 = 12;
+const inline int switch90 = 13;
 
 float readSerial(float rotation, bool identification);
 float calculateDelta(float current, float rotation);
 long calculateStepsPitch(float delta);
 long calculateStepsYaw(float delta);
+long executeSteps(long steps, int puls, bool identificator);
 
 #endif
